4617c5af7731803f5bf2b391bf91f29ca4de7d68
agustinezequielgomez <36157004+agustinezequielgomez@users.noreply.github.com>
2018-09-15 17:16:01 -0300

    Add files via upload

---
+++ Trabajo 1/Entidades/Calculadora.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// Clase que se encarga de realizar operaciones entre datos de tipo Numero
+    /// </summary>
+    public class Calculadora
+    {
+        #region Metodos
+        /// <summary>
+        /// Metodo que opera dos valores de tipo Numero segun la operacion aricmetica basica seleccionada
+        /// </summary>
+        /// <param name="num1">Primer dato de tipo Numero que sera operado</param>
+        /// <param name="num2">Segundp dato de tipo Numero que sera operado</param>
+        /// <param name="operador">Operacion aricmetica a realizar (+,-,*,/)</param>
+        /// <returns>Retorna un valor de tipo Double con el resultado de la operacion</returns>
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            double retorno = 0;
+            switch (ValidarOperador(operador))
+            {
+                case "+":
+                    retorno = num1 + num2;
+                    break;
+                case "-":
+                    retorno = num1 - num2;
+                    break;
+                case "*":
+                    retorno = num1 * num2;
+                    break;
+                case "/":
+                    retorno = num1 / num2;
+                    break;
+                default:
+                    break;
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// Metodo que valida que el operador ingresado sea correcto
+        /// </summary>
+        /// <param name="operador">Operador a validar</param>
+        /// <returns>Retorna el operador ingresado si la validacion es correcta. Caso contrario retorna el operador +</returns>
+        private static string ValidarOperador(string operador)
+        {
+            string retorno;
+            if(operador == "+" || operador == "-" || operador == "*" || operador == "/")
+            {
+                retorno = operador;
+            }
+            else
+            {
+                retorno = "+";
+            }
+            return retorno;
+        }
+        #endregion
+    }
+}
---
+++ Trabajo 1/MiCalculadora/LaCalculadora.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class LaCalculadora : Form
+    {
+        public LaCalculadora()
+        {
+            InitializeComponent();
+            string[] operadores = { "+", "-", "*", "/" };
+            foreach(string operador in operadores)
+            {
+                this.cmbOperador.Items.Add(operador);
+            }
+            this.cmbOperador.Items.Add("");
+            this.cmbOperador.SelectedItem = ("");
+            lblResultado.Text = "";
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double resultado = Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text);
+            if (!((txtNumero1.Text).All(char.IsDigit)))
+            {
+                txtNumero1.Text = "0";
+            }
+            if(!((txtNumero2.Text).All(char.IsDigit)))
+            {
+                txtNumero2.Text = "0";
+            }
+            if(cmbOperador.Text != "+" && cmbOperador.Text != "-" && cmbOperador.Text != "*" && cmbOperador.Text != "/")
+            {
+                cmbOperador.Text = "+";
+            }
+            lblResultado.Text = resultado.ToString();
+        }
+        
+        private static double Operar(string Numero1, string Numero2, string operador)
+        {
+            Numero numeroUno = new Numero(Numero1);
+            Numero numeroDos = new Numero(Numero2);
+            return Calculadora.Operar(numeroUno, numeroDos, operador);
+        }
+
+        private void Limpiar()
+        {
+            foreach (Control item in this.Controls)
+            {
+                if (item is TextBox || item is ComboBox || item is Label)
+                {
+                    item.Text = "";
+                }
+            }
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = Numero.DecimalBinario(txtNumero1.Text);
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = Numero.BinarioDecimal(txtNumero1.Text);
+        }
+    }
+}
