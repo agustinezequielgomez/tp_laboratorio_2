3c863f84e5791af3cc14e153ed7b08c132877aea
agustinezequielgomez <agusgomez99@gmail.com>
2018-09-15 22:23:36 -0300

    Comentarios agregados. Improved conversion

--- Trabajo 1/MiCalculadora/LaCalculadora.cs
+++ Trabajo 1/MiCalculadora/LaCalculadora.cs
@@ -24,26 +24,43 @@ namespace MiCalculadora
             this.cmbOperador.Items.Add("");
             this.cmbOperador.SelectedItem = ("");
             lblResultado.Text = "";
+            btnConvertirABinario.Enabled = false;
+            btnConvertirADecimal.Enabled = false;
         }
 
+        /// <summary>
+        /// Metodo que cierra el programa al hacer click en el boton cerrar;
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnCerrar_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        /// <summary>
+        /// Metodo que limpia el texto ingresado por el usuario en los textbox, combobox y label de resultado
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnLimpiar_Click(object sender, EventArgs e)
         {
             this.Limpiar();
         }
 
+        /// <summary>
+        /// Metodo que realiza una operacion al hacer click en el boton operar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnOperar_Click(object sender, EventArgs e)
         {
             double resultado = Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.Text);
-            if (!((txtNumero1.Text).All(char.IsDigit)))
+            if (!((txtNumero1.Text).All(char.IsDigit))||txtNumero1.Text == "")
             {
                 txtNumero1.Text = "0";
             }
-            if(!((txtNumero2.Text).All(char.IsDigit)))
+            if(!((txtNumero2.Text).All(char.IsDigit))||txtNumero2.Text == "")
             {
                 txtNumero2.Text = "0";
             }
@@ -52,8 +69,17 @@ namespace MiCalculadora
                 cmbOperador.Text = "+";
             }
             lblResultado.Text = resultado.ToString();
+            btnConvertirABinario.Enabled = true;
+            btnConvertirADecimal.Enabled = true;
         }
         
+        /// <summary>
+        /// Metodo que opera los valores ingresados
+        /// </summary>
+        /// <param name="Numero1">Primer numero de tipo string</param>
+        /// <param name="Numero2">Segundo numero de tipo string</param>
+        /// <param name="operador">Caracter que representa la operacion a realizar</param>
+        /// <returns>Retorna el resultado de la operacion</returns>
         private static double Operar(string Numero1, string Numero2, string operador)
         {
             Numero numeroUno = new Numero(Numero1);
@@ -61,6 +87,9 @@ namespace MiCalculadora
             return Calculadora.Operar(numeroUno, numeroDos, operador);
         }
 
+        /// <summary>
+        /// Metodo que limpia el texto ingresado por el usuario en los textbox, combobox y label de resultado
+        /// </summary>
         private void Limpiar()
         {
             foreach (Control item in this.Controls)
@@ -70,16 +99,33 @@ namespace MiCalculadora
                     item.Text = "";
                 }
             }
+            btnConvertirABinario.Enabled = false;
+            btnConvertirADecimal.Enabled = false;
         }
 
+        /// <summary>
+        /// Metodo que se encarga de llamar al conversor Decimal/Binario
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnConvertirABinario_Click(object sender, EventArgs e)
         {
-            lblResultado.Text = Numero.DecimalBinario(txtNumero1.Text);
+            lblResultado.Text = Numero.DecimalBinario(lblResultado.Text);
+            if(lblResultado.Text != "Valor invalido")
+            {
+                btnConvertirABinario.Enabled = false;
+            }
         }
 
+        /// <summary>
+        /// Metodo que se encarga de llamar al conversor Binario/Decimal
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnConvertirADecimal_Click(object sender, EventArgs e)
         {
-            lblResultado.Text = Numero.BinarioDecimal(txtNumero1.Text);
+            lblResultado.Text = Numero.BinarioDecimal(lblResultado.Text);
+            btnConvertirABinario.Enabled = true;
         }
     }
 }
